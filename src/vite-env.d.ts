fea501df695b4fc543de1e9e250c35f02a24535b
User <user@Users-iMac.local>
2024-08-04 19:15:20 +0530

    Updated imprtmeta .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,12 @@
+// vite-env.d.ts
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_TMDB_API_KEY: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  readonly VITE_TMDB_API_KEY: string;
+  readonly VITE_TMDB_BASE_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
